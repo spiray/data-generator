82c5c96dcd2eedec535b2676c7c5f8193db6ed49
Yossi Spira <spira.joe214@gmail.com>
2020-05-13 08:15:11 -0400

    feat: added write to file feature

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,12 +2,14 @@ extern crate reqwest;
 
 use std::env;
 use std::fs;
+use std::io;
+use std::io::prelude::*;
 use std::process;
 
 mod constants;
 mod helpers;
 
-fn main() {
+fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         print!("This app requires one argument to start.");
@@ -23,18 +25,26 @@ fn main() {
         process::exit(1);
     }
 
-    fs::create_dir("data");
-
-    match reqwest::get(&(constants::BASE_URL.to_string() + category)) {
-        Ok(mut response) => {
-            if response.status() == reqwest::StatusCode::OK {
-                match response.text() {
-                    Ok(text) => println!("Response text {}", text),
-                    Err(_) => println!("Can not get response text"),
+    Ok(match fs::create_dir("data") {
+        Ok(_) => {
+            match reqwest::get(&(constants::BASE_URL.to_string() + category)) {
+                Ok(mut response) => {
+                    if response.status() == reqwest::StatusCode::OK {
+                        match response.text() {
+                            Ok(text) => {
+                                // Write to file
+                                let mut file = fs::File::create(
+                                    &("./data/".to_string() + category + ".json"),
+                                )?;
+                                file.write_all(text.as_bytes())?;
+                            }
+                            Err(_) => println!("Can not get response text"),
+                        }
+                    }
                 }
+                Err(_) => println!("Request Failed!"),
             }
         }
-        Err(_) => println!("Request Failed!"),
-    }
-    // Write to file
+        Err(_) => println!("Couldn't create data directory..."),
+    })
 }
