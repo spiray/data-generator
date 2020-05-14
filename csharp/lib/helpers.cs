12a24ae450b73aae38f9331d219dde89aef2bf04
Yossi Spira <spira.joe214@gmail.com>
2020-05-14 09:55:54 -0400

    fix: moved http client to class instance

--- csharp/lib/helpers.cs
+++ csharp/lib/helpers.cs
@@ -8,14 +8,14 @@ namespace DataGenerator.Lib
 {
     public static class Helpers
     {
+        private static readonly HttpClient _client = new HttpClient();
         public static bool IsValidCategory(string category)
         {
             return Constants.ALLOWED_CATEGORIES.Contains(category);
         }
         public static async Task<string> GetJSONData(string category)
         {
-            var client = new HttpClient();
-            var response = await client.GetAsync($"{Constants.BASE_URL}{category}");
+            var response = await _client.GetAsync($"{Constants.BASE_URL}{category}");
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadAsStringAsync();
         }
