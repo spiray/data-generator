caa6e10c33b1033397249fa848c31bafacafa667
Yossi Spira <spira.joe214@gmail.com>
2020-05-19 10:13:04 -0400

    fix: added success log for all langs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -12,39 +12,40 @@ mod helpers;
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        print!("This app requires one argument to start.");
+        eprint!("This app requires one argument to start.");
         process::exit(1);
     }
     let category = &args[1];
 
     if !helpers::is_valid_category(&category) {
-        println!(
+        eprintln!(
             "This app requires one valid argument to start. The category options are - {}",
             constants::ALLOWED_CATEGORIES.join(" | ")
         );
         process::exit(1);
     }
-
-    Ok(match fs::create_dir("data") {
-        Ok(_) => {
-            match reqwest::get(&(constants::BASE_URL.to_string() + category)) {
-                Ok(mut response) => {
-                    if response.status() == reqwest::StatusCode::OK {
-                        match response.text() {
-                            Ok(text) => {
-                                // Write to file
-                                let mut file = fs::File::create(
-                                    &("./data/".to_string() + category + ".json"),
-                                )?;
-                                file.write_all(text.as_bytes())?;
-                            }
-                            Err(_) => println!("Can not get response text"),
+    fs::create_dir("data");
+    Ok(
+        match reqwest::get(&(constants::BASE_URL.to_string() + category)) {
+            Ok(mut response) => {
+                if response.status() == reqwest::StatusCode::OK {
+                    match response.text() {
+                        Ok(text) => {
+                            // Write to file
+                            let mut file =
+                                fs::File::create(&("./data/".to_string() + category + ".json"))?;
+                            file.write_all(text.as_bytes())?;
+                            eprintln!(
+                                "Check out your {} data @ /data/{}.json",
+                                &category[0..String::from(category).len() - 1],
+                                &category
+                            );
                         }
+                        Err(_) => eprintln!("Can not get response text"),
                     }
                 }
-                Err(_) => println!("Request Failed!"),
             }
-        }
-        Err(_) => println!("Couldn't create data directory..."),
-    })
+            Err(_) => eprintln!("Request Failed!"),
+        },
+    )
 }
